431c4aba4737031ee544193aff5e51d768af45ca
veniamin <venya957@gmail.com>
2024-05-07 09:59:01 +0300

    initial

--- catalog/models.py
+++ catalog/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=100),
-    description = models.TextField(),
+    name = models.CharField(max_length=100, verbose_name='Наименованик'),
+    description = models.TextField(verbose_name='Описание'),
 
     class Meta:
         verbose_name = 'Категория'
@@ -15,12 +15,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100),
-    description = models.TextField(),
-    image = models.ImageField(upload_to='/templates/'),
+    name = models.CharField(max_length=100, verbose_name='Наименование'),
+    description = models.TextField(verbose_name='Описание'),
+    image = models.ImageField(upload_to='catalog/', verbose_name='Изображение', null=True, blank=True),
     category = models.ForeignKey(Category, on_delete=models.CASCADE),
-    created_at = models.DateField(auto_now_add=True),
-    updated_at = models.DateField(auto_now=True),
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания'),
+    updated_at = models.DateField(auto_now=True, verbose_name='Дата последнего изменения'),
 
     class Meta:
         verbose_name = 'Товар'
--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 STATCFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
