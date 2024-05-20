43dc755a8840992dd158d241b21b2ba970737f2c
veniamin <venya957@gmail.com>
2024-05-20 18:24:09 +0300

    Merge remote-tracking branch 'main/main'

--- blogentry/models.py
+++ blogentry/models.py
@@ -8,6 +8,7 @@ class Blog(models.Model):
     preview = models.ImageField(upload_to='blogentry/photo', verbose_name='Изображение', null=True, blank=True)
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     sign_publication = models.IntegerField(default=0, verbose_name='Просмотры')
+    is_published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
