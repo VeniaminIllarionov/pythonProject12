de154cf78fad6f26a67d0ac72c8bb56e892888d4
veniamin <venya957@gmail.com>
2024-05-20 17:30:24 +0300

    Merge remote-tracking branch 'main/main'

--- blogentry/models.py
+++ blogentry/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100, verbose_name="Заголовок")
     content = models.TextField(verbose_name='Описание')
-    slug = models.CharField(max_length=150, verbose_name='Slug')
+    slug = models.CharField(max_length=150, verbose_name='Slug', null=True, blank=True)
     preview = models.ImageField(upload_to='blogentry/photo', verbose_name='Изображение', null=True, blank=True)
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     sign_publication = models.IntegerField(default=0, verbose_name='Просмотры')
